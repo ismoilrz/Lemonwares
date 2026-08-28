214cd595958157c870ffec5438beb579ab786749
ismoilrz <ismoil4766451155@gmail.com>
2026-08-28 16:01:32 +0300

    g

---
+++ utils/localData.js
@@ -0,0 +1,263 @@
+const btns = document.querySelectorAll('.viewBtn')
+const contents = document.querySelectorAll('.contentView')
+
+const hostingLogos = document.getElementById('hostingLogos')
+const hostingBuild = document.getElementById('hostingBuild')
+
+const chack = document.getElementById('chackbox')
+const startedBtn = document.querySelectorAll('.startedBtn')
+const startedCon = document.querySelectorAll('.startedCon')
+const basicCon = document.getElementById('basicCon')
+const premiumCon = document.getElementById('premiumCon')
+
+
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btns.forEach(btn => btn.classList.remove('active'))
+        contents.forEach(cnt => cnt.classList.remove('active'))
+        
+        btn.classList.add('active')
+
+        const btnTar = btn.getAttribute('data-view')
+        document.getElementById(btnTar).classList.add('active')
+    })
+})
+
+// ///////////////////// Hosting data 
+
+const HostingLogos = [
+    {
+        id: 1,
+        url: './assets/webHostingLogo1.svg',
+        name: 'cPanel',
+    },
+    {
+        id: 2,
+        url: './assets/webHostingLogo2.svg',
+        name: 'immunify 360',
+    },
+    {
+        id: 3,
+        url: './assets/webHostingLogo3.svg',
+        name: 'LITESPEED',
+    },
+    {
+        id: 4,
+        url: './assets/webHostingLogo4.svg',
+        name: 'Softaculous',
+    },
+    {
+        id: 5,
+        url: './assets/webHostingLogo5.svg',
+        name: 'WordPress',
+    },
+    {
+        id: 6,
+        url: './assets/webHostingLogo6.svg',
+        name: 'CLOUDFLARE',
+    }
+]
+
+const HostingBuild = [
+    {
+        id: 1,
+        url: './assets/hostingBuild1.png',
+        title: '99.9% Uptime',
+        description: 'We Keep Your Web build Online 24x7x365. Downtime not only costs you lost visitors but also damages your reputation and search engine rankings.'
+    },
+    {
+        id: 2,
+        url: './assets/hostingBuild2.png',
+        title: 'Blazing Fast Web Hosting',
+        description: 'Downtime not only costs you lost visitors but also damages your reputation and search engine rankings.'
+    }, {
+        id: 3,
+        url: './assets/hostingBuild3.png',
+        title: 'Free SSL Certificates',
+        description: 'We Keep Your Web build Online 24x7x365. Downtime not only costs you lost visitors but also damages your reputation and search engine rankings.'
+    },
+    {
+        id: 4,
+        url: './assets/hostingBuild4.png',
+        title: '24x7 Friendly Support',
+        description: 'We Keep Your Web build Online 24x7x365. Downtime not only costs you lost visitors but also damages your reputation and search engine rankings.'
+    }
+]
+
+hostingLogos.innerHTML = HostingLogos.map(logos => 
+    `<a href="#" class="w-[26%]" key=${logos.id}>
+    <img class="w-full h-full object-contain" src="${logos.url}" alt="${logos.name}" />
+    </a>`
+).join('')
+
+hostingBuild.innerHTML = HostingBuild.map(build => 
+    `<div class="w-1/5 flex flex-col items-center gap-3" key="${build.id}">
+    <img class="w-25" h-25 src="${build.url}" />
+    <h5 class="text-[20px] font-bold text-black">${build.title}</h5>
+    <p class="text-center text-[12px] text-black">${build.description}<p/>
+    </div>`
+).join('')
+
+chack.addEventListener('click', () => {
+    const isActive = chack.classList.toggle('active')
+    chack.setAttribute('chackcircle', isActive)
+})
+
+// ///////////////////// Started data 
+
+const StartedBasic = [
+    {
+        id: 1,
+        title: 'Starter',
+        description: 'with all your customers via all conversation channels in one central dashboard.',
+        price: '2.80',
+        advance: {
+            ssd: '2GB SSD',
+            bandwidth: '10GB Bandwidth',
+            accounts: '15 Email Accounts',
+            database: 'Unlimited Database',
+            subdomains: '4 Subdomains',
+            parked: '1 Parked Domain',
+            website: '2 Websites',
+            ssl: 'Free SSL',
+            softaculous: 'Softaculous'
+        },
+    },
+    {
+        id: 2,
+        title: 'Standart',
+        description: 'with all your customers via all conversation channels in one central dashboard.',
+        price: '4.20',
+        advance: {
+            ssd: '5GB SSD',
+            bandwidth: '25GB Bandwidth',
+            accounts: '30 Email Accounts',
+            database: 'Unlimited Database',
+            subdomains: '10 Subdomains',
+            parked: '3 Parked Domain',
+            website: '5 Websites',
+            ssl: 'Free SSL',
+            softaculous: 'Softaculous'
+        },
+    },
+    {
+        id: 3,
+        title: 'Suprem',
+        description: 'with all your customers via all conversation channels in one central dashboard.',
+        price: '7.00',
+        advance: {
+            ssd: '15GB SSD',
+            bandwidth: '60GB Bandwidth',
+            accounts: '60 Email Accounts',
+            database: 'Unlimited Database',
+            subdomains: '20 Subdomains',
+            parked: '8 Parked Domain',
+            website: '10 Websites',
+            ssl: 'Free SSL',
+            softaculous: 'Softaculous'
+        },
+    },
+]
+
+const StartedPremium = [
+    {
+        id: 1,
+        title: 'Pro',
+        description: 'with all your customers via all conversation channels in one central dashboard.',
+        price: '20.00',
+        advance: {
+            ssd: '50GB SSD',
+            bandwidth: '200GB Bandwidth',
+            accounts: '200 Email Accounts',
+            database: 'Unlimited database',
+            subdomains: '60 Subdomains',
+            parked: '25 Parked Domain',
+            website: '30 Websites',
+            ssl: 'Free SSL',
+            softaculous: 'Softaculous',
+        }
+    },
+    {
+        id: 2,
+        title: 'Plus',
+        description: 'with all your customers via all conversation channels in one central dashboard.',
+        price: '50.00',
+        advance: {
+            ssd: '130GB SSD',
+            bandwidth: '500GB Bandwidth',
+            accounts: '500 Email Accounts',
+            database: 'Unlimited database',
+            subdomains: '150 Subdomains',
+            parked: '65 Parked Domain',
+            website: '80 Websites',
+            ssl: 'Free SSL',
+            softaculous: 'Softaculous',
+        }
+    },
+]
+
+
+// ///////////////////////// started
+
+startedBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        startedBtn.forEach(btn => btn.classList.remove('active'))
+        startedCon.forEach(cnt => cnt.classList.remove('active'))
+
+        btn.classList.add('active')
+
+        const startedTar = btn.getAttribute('startedTab')
+        document.getElementById(startedTar).classList.add('active')
+    })
+})
+
+
+
+basicCon.innerHTML = StartedBasic.map(strt => 
+    `<div key="${strt.id}" class="w-[300px] px-[30px] py-[20px] border-2 border-solid border-[#C4C4C4] rounded-[15px]">
+        <h5 class="text-[28px] font-[600]">${strt.title}</h5>
+        <p class="max-w-[85%] mt-1 mb-3 text-[12px]">${strt.description}</p>
+        <strong class="text-[42px] font-bold">$${strt.price}</strong>
+        <p class="text-[12px]">Per month</p>
+        <button class="w-full h-[50px] border-2 border-solid border-black mx-auto rounded-[12px] text-[14px] font-medium my-[35px]">
+            Choose this plan
+        </button>
+        <div class="flex flex-col gap-[15px]">
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.ssd}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.bandwidth}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.accounts}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.database}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.subdomains}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.parked}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.website}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.ssl}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${strt.advance.softaculous}</span></div>
+        </div>
+    </div>`
+).join('')
+
+
+premiumCon.innerHTML = StartedPremium.map(pr => 
+       `<div key="${pr.id}" class="w-[300px] px-[30px] py-[20px] border-2 border-solid border-[#C4C4C4] rounded-[15px]">
+        <h5 class="text-[28px] font-[600]">${pr.title}</h5>
+        <p class="max-w-[85%] mt-1 mb-3 text-[12px]">${pr.description}</p>
+        <strong class="text-[42px] font-bold">$${pr.price}</strong>
+        <p class="text-[12px]">Per month</p>
+        <button class="w-full h-[50px] border-2 border-solid border-black mx-auto rounded-[12px] text-[14px] font-medium my-[35px]">
+            Choose this plan
+        </button>
+        <div class="flex flex-col gap-[15px]">
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.ssd}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.bandwidth}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.accounts}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.database}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.subdomains}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.parked}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.website}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.ssl}</span></div>
+            <div class="flex gap-6 w-full"><img src="./assets/Checkmark.svg" /> <span class="text-[16px] font-bold text-[#808080]">${pr.advance.softaculous}</span></div>
+        </div>
+    </div>`
+).join('')
